46b33552d827cc78acb852490596be186d93249a
roman <ravoro@gmail.com>
2016-11-08 19:55:00 +0000

    base - play framework skeleton

---
+++ app/controllers/HomeController.scala
@@ -0,0 +1,13 @@
+package controllers
+
+import javax.inject._
+
+import play.api.i18n.{I18nSupport, MessagesApi}
+import play.api.mvc._
+
+@Singleton
+class HomeController @Inject()(val messagesApi: MessagesApi) extends Controller with I18nSupport {
+  def get = Action {
+    Ok(views.html.home())
+  }
+}
