37ae934b000d3b65a5f88d5798b3962744af2f5b
roman <ravoro@gmail.com>
2016-11-17 13:52:18 +0000

    mvp - improve edit form. enable updating

--- test/controllers/WordControllerSpec.scala
+++ test/controllers/WordControllerSpec.scala
@@ -11,7 +11,6 @@ import play.api.test.FakeRequest
 import play.api.test.Helpers._
 import play.api.{Configuration, Environment}
 import repositories.WordRepository
-import repositories.WordRepository.DuplicateRecordException
 
 import scala.util.{Failure, Try}
 
@@ -22,52 +21,74 @@ class WordControllerSpec extends PlaySpec with MockitoSugar {
     new DefaultLangs(Configuration.reference))
   val mockRepo = mock[WordRepository]
 
-  def buildController(messagesApi: MessagesApi = mockMessagesApi,
-                      wordsRepo: WordRepository = mockRepo) = {
+  def buildController(wordsRepo: WordRepository = mockRepo,
+                      messagesApi: MessagesApi = mockMessagesApi) = {
     new WordController(messagesApi, wordsRepo)
   }
 
-  "addGet" must {
-    "return OK and display the add word form" in {
-      val result = buildController().addGet()(FakeRequest())
+  val mockWord = Word("hello", "type of greeting")
+
+  "editForm" must {
+    "return OK and display an empty word form if no custom definition exists" in {
+      val mockRepo = mock[WordRepository]
+      when(mockRepo.get(any())).thenReturn(None)
+      val result = buildController(mockRepo).editForm(mockWord.word)(FakeRequest())
       status(result) mustBe OK
       val doc = Jsoup.parse(contentAsString(result))
-      doc.getElementById("form-word-add") must not be null
+      doc.getElementsByTag("textarea").size mustBe 1
+      doc.getElementsByTag("textarea").get(0).text() mustBe ""
+    }
+
+    "return OK and display an empty word form if a custom definition exists, but it is blank" in {
+      val mockRepo = mock[WordRepository]
+      when(mockRepo.get(any())).thenReturn(Some(mockWord.copy(definition = "")))
+      val result = buildController(mockRepo).editForm(mockWord.word)(FakeRequest())
+      status(result) mustBe OK
+      val doc = Jsoup.parse(contentAsString(result))
+      doc.getElementsByTag("textarea").size mustBe 1
+      doc.getElementsByTag("textarea").get(0).text() mustBe ""
+    }
+
+    "return OK and display a prefilled form with the existing custom definition" in {
+      val mockRepo = mock[WordRepository]
+      when(mockRepo.get(any())).thenReturn(Some(mockWord))
+      val result = buildController(mockRepo).editForm(mockWord.word)(FakeRequest())
+      status(result) mustBe OK
+      val doc = Jsoup.parse(contentAsString(result))
+      doc.getElementsByTag("textarea").size mustBe 1
+      doc.getElementsByTag("textarea").get(0).text() mustBe mockWord.definition
     }
   }
 
-  "addPost" must {
-    val mockSubmission = Word("hello", "type of greeting")
-    val mockFormData = Map("word" -> mockSubmission.word, "definition" -> mockSubmission.definition)
+  "edit" must {
+    val mockFormData = Map("definition" -> mockWord.definition)
 
-    "return OK, add new word to db and redirect to the newly created word's page" in {
+    "return OK, upsert the word to db and redirect to the newly created word's page" in {
       val mockRepo = mock[WordRepository]
-      when(mockRepo.add(any(), any())).thenReturn(Try(()))
+      when(mockRepo.upsert(any(), any())).thenReturn(Try(()))
       val request = FakeRequest().withFormUrlEncodedBody(mockFormData.toSeq: _*)
-      val result = buildController(wordsRepo = mockRepo).addPost()(request)
+      val result = buildController(wordsRepo = mockRepo).edit(mockWord.word)(request)
       status(result) mustBe SEE_OTHER
-      verify(mockRepo).add(mockSubmission.word, mockSubmission.definition)
-      await(result).header.headers("Location") mustBe routes.WordController.get(mockSubmission.word).url
+      verify(mockRepo).upsert(mockWord.word, mockFormData("definition"))
+      await(result).header.headers("Location") mustBe routes.WordController.get(mockWord.word).url
+      flash(result).get("message") mustBe Some(s"""Successfully updated definition of "${mockWord.word}".""")
     }
 
-    "return BAD_REQUEST and display the add word form with errors when an invalid submission is made" in {
-      val invalidData = mockFormData - "definition"
-      val request = FakeRequest().withFormUrlEncodedBody(invalidData.toSeq: _*)
-      val result = buildController().addPost()(request)
-      status(result) mustBe BAD_REQUEST
-      val doc = Jsoup.parse(contentAsString(result))
-      doc.getElementById("form-word-add") must not be null
-      doc.getElementsByClass("error").size must be > 0
+    "return BAD_REQUEST and display the edit form with errors when an invalid submission is made" in {
+      pending
     }
 
-    "return BAD_REQUEST and display the add word form when the submitted word is already in the db" in {
+    "return INTERNAL_SERVER_ERROR and redisplay the form with an error when an error occurs during saving" in {
       val mockRepo = mock[WordRepository]
-      when(mockRepo.add(any(), any())).thenReturn(Failure(new DuplicateRecordException("")))
+      when(mockRepo.upsert(any(), any())).thenReturn(Failure(new Exception))
       val request = FakeRequest().withFormUrlEncodedBody(mockFormData.toSeq: _*)
-      val result = buildController(wordsRepo = mockRepo).addPost()(request)
-      status(result) mustBe BAD_REQUEST
+      val result = buildController(wordsRepo = mockRepo).edit(mockWord.word)(request)
+      status(result) mustBe INTERNAL_SERVER_ERROR
+      verify(mockRepo).upsert(mockWord.word, mockFormData("definition"))
       val doc = Jsoup.parse(contentAsString(result))
-      doc.getElementById("form-word-add") must not be null
+      doc.getElementById("edit-form") must not be null
+      doc.getElementsByClass("alert-danger").size mustBe 1
+      doc.getElementsByClass("alert-danger").get(0).text mustBe "Error occurred while saving, please try again."
     }
   }
 }
